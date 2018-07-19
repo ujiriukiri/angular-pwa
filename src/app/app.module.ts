fd6d120c47e3a181b0e8a0dfd4b2aa614d38040d
Ujiri Ukiri <ujiri.ukiri@cloud9.ng>
2018-07-19 18:24:57 +0100

    import angular-material theme components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import 'hammerjs';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
