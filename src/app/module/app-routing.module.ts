5a2851611caa79c7a7ef7a65e5185d6fd5fec6be
Ujiri Ukiri <ujiri.ukiri@cloud9.ng>
2018-07-24 13:50:29 +0100

    Add history page component

--- src/app/module/app-routing.module.ts
+++ src/app/module/app-routing.module.ts
@@ -11,12 +11,14 @@ import { ListComponent } from '../component/list/list.component';
 import { VerificationComponent } from '../component/verification/verification.component';
 import { SignupComponent } from '../component/signup/signup.component';
 import { OrderDetailComponent } from '../component/order-detail/order-detail.component';
+import { HistoryComponent } from '../component/history/history.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'map/:id', component: MapComponent },
   { path: 'list', component: ListComponent },
+  { path: 'history', component: HistoryComponent },
   { path: 'verification', component: VerificationComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'order/:id', component: OrderDetailComponent }
@@ -33,5 +35,5 @@ export class AppRoutingModule { }
 export const routingComponents = [// AppComponent,
         MainNavComponent, TopnavComponent, SidebarComponent, DashboardComponent, MapComponent,
         ListComponent, VerificationComponent, SignupComponent,
-        OrderDetailComponent
+        OrderDetailComponent, HistoryComponent
 ];
