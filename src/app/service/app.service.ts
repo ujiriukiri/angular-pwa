8c28f1cdda3439d1ab8aa6894c236d91cea01bed
Ujiri Ukiri <ujiri.ukiri@cloud9.ng>
2018-07-25 11:59:20 +0100

    Initialize shareeable service to hold app info

---
+++ src/app/service/app.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+// import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+  app_info_init = {
+    title: 'Dispatch',
+    user: {}
+  };
+
+  // private APPINFO = new BehaviorSubject<any>(this.app_info_init);
+  // currentAppInfo = this.APPINFO.asObservable();
+
+  constructor() {}
+
+  updateAppInfo(app_info) {
+    // this.APPINFO.next(app_info);
+  }
+}
