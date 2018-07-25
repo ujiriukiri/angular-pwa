11b67effed4a61e4ed564106dd4c7434876fc580
Ujiri Ukiri <ujiri.ukiri@cloud9.ng>
2018-07-25 11:57:55 +0100

    Get all orders and specific order from dataservice

--- src/app/component/list/list.component.ts
+++ src/app/component/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { DataService } from '../../service/data.service';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  orders: any = [];
+  by_time: any = [];
+  by_priority: any = [];
+  by_distance: any = [];
+
+  @Output() appInfoEvent = new EventEmitter<any>();
+  app: any = {};
+
+  constructor(
+    private data: DataService,
+  ) { }
 
   ngOnInit() {
+    this.orders = this.data.getList();
+    this.by_priority = this.orders.map((item) => {
+      if (item.status === 1) {
+        this.by_time.push(item);
+      }
+      if (item.status === 2) {
+        this.by_distance.push(item);
+      }
+      if (item.status === 3) {
+        this.by_priority.push(item);
+      }
+    });
+  }
+
+  changeAppInfo() {
+    console.log(this.app);
+    this.app = {
+      title: 'Dispatch App'
+    };
+    console.log(this.app);
+    this.appInfoEvent.emit(this.app);
   }
 
 }
